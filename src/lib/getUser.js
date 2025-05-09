cb3a70a2d092e346de3977f6960f8a3dbdb1b02f
Charles Green <hello@charles.green>
2025-05-09 12:06:56 +0900

    codelab end point

---
+++ src/lib/getUser.js
@@ -0,0 +1,18 @@
+"use client";
+
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+
+import { auth } from "@/src/lib/firebase/clientApp.js";
+
+export function useUser() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    return onAuthStateChanged(auth, (authUser) => {
+      setUser(authUser);
+    });
+  }, []);
+
+  return user;
+}
